86f5d5c786dd19b05abe4683afc6bdc2443a7a1b
arrogantlynx <arrogantlynx@gmail.com>
2019-11-05 01:22:47 +0530

    added date pipe to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DatePipe } from '@angular/common';
+
+
 @NgModule({
   declarations: [
     AppComponent
