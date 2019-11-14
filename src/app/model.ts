1d36d17ae99950062cf4306bee0f49da78675824
arrogantlynx <arrogantlynx@gmail.com>
2019-11-14 11:14:45 +0530

    added TypeScript class

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,3 +1,4 @@
+/*
 let model = {
 user: 'Adam',
 items:
@@ -8,3 +9,33 @@ items:
 {action: 'Call Joe', done: false}
 ]
 };
+*/ // commented out to use typescript class instead of javascript object
+
+export class Model {
+
+    user;
+    items;
+
+    constructor() {
+
+        this.user = 'Adam';
+
+        this.items = [ new TodoItem('Buy Flowers', false) ,
+        new TodoItem('Get Shoes', false) ,
+        new TodoItem('Collect Tickets', false) ,
+        new TodoItem('Call Joe', false) ];
+
+    }
+
+}
+
+export class TodoItem {
+
+    action;
+    done;
+
+    constructor(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+}
